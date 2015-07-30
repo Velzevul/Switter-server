4ff2320f3d2cbc8095e22b0b78a740f75a9057e5
Volodymyr Dziubak <volodymyr.dzyubak@gmail.com>
2015-07-30 17:50:59 -0500

    more changes

--- client/views.py
+++ client/views.py
@@ -4,4 +4,4 @@ from django.views.decorators.csrf import ensure_csrf_cookie
 
 @ensure_csrf_cookie
 def index(request):
-    return render(request, 'client/index.html')
+    return render(request, 'index.html')
