cd63373d264097dd183db62eaf3e1960adb632cb
Volodymyr Dziubak <volodymyr.dzyubak@gmail.com>
2015-08-21 12:12:19 -0500

    added logger app

---
+++ logger/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Log(models.Model):
+    participant_id = models.CharField(max_length=255)
+    message = models.CharField(max_length=255)
+    created_at = models.DateTimeField()
+
+    def __str__(self):
+        return self.message
