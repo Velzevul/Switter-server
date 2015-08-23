4d12a33420a03400268c1c0afe20711db78295fd
Volodymyr Dziubak <volodymyr.dzyubak@gmail.com>
2015-08-23 17:48:32 -0500

    modified CRON task to incorporate different times of the day

--- task.py
+++ task.py
@@ -1,7 +1,8 @@
 from django.utils import timezone
 from datetime import datetime
 from random import random
-import os, sys
+import os
+import sys
 
 PROJECT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__)))
 sys.path += [PROJECT_PATH]
@@ -11,24 +12,36 @@ from api.models import Tweet
 
 
 def task():
+    now = datetime.now()
     t = Tweet.objects.filter(is_deployed__exact=False).first()
     non_deployed_tweets_left = Tweet.objects.filter(is_deployed__exact=False).count()
     if t is None:
-        print('{0}: No more tweets to deploy!'.format(datetime.now().ctime()))
+        print('{0}: No more tweets to deploy!'.format(now.ctime()))
     else:
+        if now.hour < 8:
+            threshold = 0.8
+        elif now.hour < 12:
+            threshold = 0.65
+        elif now.hour < 16:
+            threshold = 0.5
+        elif now.hour < 21:
+            threshold = 0.65
+        else:
+            threshold = 0.8
+
         chance = random()
-        threshold = 0.6
         if chance > threshold:
             t.created_at = timezone.now()
             t.is_deployed = True
             t.save()
             non_deployed_tweets_left -= 1
-            print('{0}: Tweet {1} has been deployed. Non-deployed tweets left: {2}'.format(datetime.now().ctime(), t.id, non_deployed_tweets_left))
+            print('{0}: (chance - {1}%) Tweet {2} has been deployed. Non-deployed tweets left: {3}'
+                  .format(now.ctime(), threshold*100, t.id, non_deployed_tweets_left))
             # for JSON return, that's how to get the time in the current (correct) timezone:
             # t.created_at.astimezone(timezone.get_current_timezone()).ctime()
         else:
-            print('{0}: Chance check failed, not deploying. Non-deployed tweets left: {1}'.format(datetime.now().ctime(), non_deployed_tweets_left))
-
+            print('{0}: (chance - {1}%) Chance check failed, not deploying. Non-deployed tweets left: {2}'
+                  .format(now.ctime(), threshold*100, non_deployed_tweets_left))
 
 
 if __name__ == '__main__':
