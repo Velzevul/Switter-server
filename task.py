fcb4a62aa6f24724dca2e5fd1cf7ff3b44d44a2c
Volodymyr Dziubak <volodymyr.dzyubak@gmail.com>
2015-08-28 21:22:00 -0500

    more weight tweaking

--- task.py
+++ task.py
@@ -19,15 +19,15 @@ def task():
         print('{0}: No more tweets to deploy!'.format(now.ctime()))
     else:
         if now.hour < 8:
-            threshold = 0.9
+            threshold = 0.85
         elif now.hour < 12:
-            threshold = 0.75
+            threshold = 0.73
         elif now.hour < 16:
-            threshold = 0.65
+            threshold = 0.6
         elif now.hour < 21:
-            threshold = 0.75
+            threshold = 0.73
         else:
-            threshold = 0.9
+            threshold = 0.85
 
         chance = random()
         if chance > threshold:
